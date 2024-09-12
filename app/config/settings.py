fc70f2dca334d7ce6069472382c7743972e0b917
Nikhil <98060511+NikAtNight@users.noreply.github.com>
2024-09-12 13:16:45 -0400

    added auth and containers

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,13 +1,15 @@
 import os
 import dj_database_url
+from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(BASE_DIR)
 
 DJANGO_DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 ROOT_URLCONF = 'app.urls'
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', "django-insecure-vkto8gdmiv=@3(ki%wza!2y1vurui4k_z*q&rm!m7w!y6*9ep^")
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 WSGI_APPLICATION = 'app.wsgi.application'
 X_FRAME_OPTIONS = 'DENY'
 
@@ -24,7 +26,6 @@ USE_TZ = True
 # CORS Settings
 CORS_PREFLIGHT_MAX_AGE = 300
 CORS_ORIGIN_ALLOW_ALL = True
-
 
 DATABASES = {
     'default': dj_database_url.config(
@@ -47,7 +48,6 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
     'rest_framework',                               # utilities for rest apis
     'django_filters',                               # for filtering rest endpoints
-    # 'django_cron',                                  # scheduled tasks with cron
     'corsheaders',                                  # CORS header injection
     # JWT blacklist functionality provider
     'rest_framework_simplejwt.token_blacklist',
@@ -56,7 +56,9 @@ THIRD_PARTY_APPS = [
     'import_export',                                # admin import / export
 ]
 
-PROJECT_APPS = []
+PROJECT_APPS = [
+    'app.users',
+]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
@@ -72,7 +74,7 @@ MIDDLEWARE = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR + "/static/"
+STATIC_ROOT = PARENT_DIR + "/static/"
 STATICFILES_DIRS = []
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
@@ -98,9 +100,7 @@ TEMPLATES = [
     },
 ]
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'users.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -116,3 +116,59 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+SIMPLE_JWT = {
+    'SLIDING_TOKEN_LIFETIME': timedelta(hours=1, minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1, minutes=5),
+
+    'AUTH_TOKEN_CLASSES': ['rest_framework_simplejwt.tokens.SlidingToken'],
+    'AUTH_HEADER_TYPES': ['Bearer', 'JWT'],
+
+    'USER_ID_CLAIM': 'userId',
+    'TOKEN_TYPE_CLAIM': 'type',
+}
+
+
+REST_FRAMEWORK = {
+    'NON_FIELD_ERRORS_KEY': 'nonFieldErrors',
+    'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S%z',
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'app.auth.providers.SupabaseTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
+CRON_CLASSES = []
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.getenv('REDIS_URL'),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "cache"
+    }
+}
+
+CELERY_BROKER_URL = os.getenv('REDIS_URL')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL')
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_IGNORE_RESULT = True
+CELERYD_TASK_SOFT_TIME_LIMIT = 300
+CELERYD_TASK_TIME_LIMIT = 500
+CELERY_BEAT_SCHEDULER = os.getenv('CELERY_BEAT_SCHEDULER')
+
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_JWT_SECRET = os.getenv('SUPABASE_JWT_SECRET')
+SUPABASE_JWT_ALGORITHMS = os.getenv('SUPABASE_JWT_ALGORITHMS')
+SUPABASE_JWT_ISSUER = os.getenv('SUPABASE_JWT_ISSUER')
--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,21 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_nested import routers
+
+from app.users.views import UserViewSet
+
+app_router = routers.SimpleRouter()
+
+app_router.register(r'users', UserViewSet, basename='users')
+
+combined_urls = app_router.urls
 
 urlpatterns = [
     path('skynet/doc/', include('django.contrib.admindocs.urls')),
     path('skynet/', admin.site.urls),
-    # path('api/', include((combined_urls, 'budget'), namespace='v1')),
+    path('api/', include((combined_urls, 'budget'), namespace='v1')),
 ] + static(
     settings.STATIC_URL,
     document_root=settings.STATIC_ROOT
